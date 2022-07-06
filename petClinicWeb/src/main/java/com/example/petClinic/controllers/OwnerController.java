3ebde1a785e11e9424b78a3f22fd4079ea22e9ca
Maurya-Grover <maurya.grover@gmail.com>
2022-07-06 06:07:31 +0530

    Add Owner, Vet List. closes #3

--- petClinicWeb/src/main/java/com/example/petClinic/controllers/OwnerController.java
+++ petClinicWeb/src/main/java/com/example/petClinic/controllers/OwnerController.java
@@ -1,16 +1,27 @@
 package com.example.petClinic.controllers;
 
+import com.example.petClinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/owners","/owner"})
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired // Autowired optional here as well
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
     // alternate way to have request mappings where  it is known all have a common path
     // with / and without / is treated different and hence both need to be mapped
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
--- petClinicWeb/src/main/java/com/example/petClinic/controllers/VetController.java
+++ petClinicWeb/src/main/java/com/example/petClinic/controllers/VetController.java
@@ -1,12 +1,25 @@
 package com.example.petClinic.controllers;
 
+import com.example.petClinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping({"/vet", "/vets"})
 @Controller
 public class VetController {
-    @RequestMapping({"/vets", "/vets/index.html", "/vets/index"})
-    public String listVets() {
+
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index.html", "/index"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
         // the first part has to be same name as directory name in templates directory
         // so that appropriate file can be fetched and served
